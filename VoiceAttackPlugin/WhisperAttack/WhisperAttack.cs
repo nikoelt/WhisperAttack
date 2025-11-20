17459327b1ee561821e63b6c5a27bb528b900d9e
sleighzy\simonl <giveit@gmail.com>
2025-11-21 12:49:55 +1300

    Add plugin comments

--- VoiceAttackPlugin/WhisperAttack/WhisperAttack.cs
+++ VoiceAttackPlugin/WhisperAttack/WhisperAttack.cs
@@ -7,42 +7,66 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace WhisperAttackServerCommand
+namespace WhisperAttack
 {
-    public class VA_Plugin
+    public class WhisperAttackServerCommand
     {
         private static bool _isRunning = true;
         private static TcpListener _listener = null;
         private static IPAddress _listenerIpAddress = IPAddress.Parse("127.0.0.1");
         private static int _listenerPort = 65433;
 
+        /// <summary>
+        /// The plugin’s display name, required by the VoiceAttack plugin API.
+        /// </summary>
+        /// <returns>The display name.</returns>
         public static string VA_DisplayName()
         {
             return "WhisperAttack";
         }
 
+        /// <summary>
+        /// The plugin’s description, required by the VoiceAttack plugin API.
+        /// </summary>
+        /// <returns>The description.</returns>
         public static string VA_DisplayInfo()
         {
             return "WhisperAttack Server Command plugin (v1.2.2)";
         }
 
+        /// <summary>
+        /// The plugin’s GUID, required by the VoiceAttack plugin API.
+        /// </summary>
+        /// <returns>The GUID.</returns>
         public static Guid VA_Id()
         {
             return new Guid("{1AD02372-145E-4143-BBBE-AC7575595C24}");
         }
 
+        /// <summary>
+        /// VoiceAttack calls this when a command is run to stop all running commands.
+        /// This is currently unused.
+        /// </summary>
         public static void VA_StopCommand()
         {
+            // no-op
         }
 
+        /// <summary>
+        /// Invoked when the plugin is loaded. This runs a connection test to see if the WhisperAttack
+        /// server is currently running, and starts up the listener for receiving commands.
+        /// </summary>
+        /// <param name="vaProxy">The VoiceAttack proxy for calling VoiceAttack functions</param>
         public static void VA_Init1(dynamic vaProxy)
         {
-            // Configuration for connecting to the WhisperAttack server.
-            string server = "127.0.0.1";
-            int port = 65432;
-
+            // Run a connection test to see if WhisperAttack is currently running and
+            // listening for server commands.
             try
             {
+                // Configuration for connecting to the WhisperAttack server.
+                string server = "127.0.0.1";
+                int port = 65432;
+
                 using (TcpClient client = new TcpClient(server, port))
                 using (NetworkStream stream = client.GetStream())
                 {
@@ -54,10 +78,14 @@ namespace WhisperAttackServerCommand
                 vaProxy.WriteToLog($"Failed to connect to WhisperAttack server: {ex.Message}", "red");
             }
 
-            loadConfiguration(vaProxy);
+            LoadConfiguration(vaProxy);
             StartCommandListener(vaProxy);
         }
 
+        /// <summary>
+        /// Sends the start/stop recording commands to the WhisperAttack server.
+        /// </summary>
+        /// <param name="vaProxy">The VoiceAttack proxy for calling VoiceAttack functions</param>
         public static void VA_Invoke1(dynamic vaProxy)
         {
             string server = "127.0.0.1";
@@ -98,6 +126,11 @@ namespace WhisperAttackServerCommand
             }
         }
 
+        /// <summary>
+        /// Send the shudown command to the WhisperAttack server when VoiceAttack exits.
+        /// This will close WhisperAttack so that it is no longer running.
+        /// </summary>
+        /// <param name="vaProxy">The VoiceAttack proxy for calling VoiceAttack functions</param>
         public static void VA_Exit1(dynamic vaProxy)
         {
             _isRunning = false;
@@ -115,11 +148,19 @@ namespace WhisperAttackServerCommand
             }
         }
 
-        private static void loadConfiguration(dynamic vaProxy)
+        /// <summary>
+        /// Loads the configuration from the settings.cfg file in the same location as the plugin.
+        /// Configuration is in the format key=value
+        /// Configuration specifies the ip address and port for listening on to receive text commands.
+        /// </summary>
+        /// <param name="vaProxy">The VoiceAttack proxy for calling VoiceAttack functions</param>
+        private static void LoadConfiguration(dynamic vaProxy)
         {
-            string settingsFile = "Apps\\WhisperAttackServerCommand\\settings.cfg";
-            string currentDir = Directory.GetCurrentDirectory();
-            string filePath = Path.Combine(currentDir, settingsFile);
+            // Get the VoiceAttack directory that contains plugins
+            string voiceAttackAppsDir = vaProxy.SessionState["VA_APPS"];
+            // Create the path to the WhisperAttack plugin directory and configuration file under the folder
+            // that VoiceAttack keeps plugins in.
+            string filePath = Path.Combine(voiceAttackAppsDir, "WhisperAttackServerCommand\\settings.cfg");
 
             // Read the configuration file, if it exists, in the format key=value
             if (!File.Exists(filePath))
@@ -132,7 +173,7 @@ namespace WhisperAttackServerCommand
             {
                 foreach (var line in File.ReadAllLines(filePath))
                 {
-                    // Trim whitespace and skip empty or comment lines
+                    // Trim whitespace and skip empty or commented out lines
                     var trimmed = line.Trim();
                     if (string.IsNullOrEmpty(trimmed) || trimmed.StartsWith("#"))
                         continue;
@@ -149,6 +190,7 @@ namespace WhisperAttackServerCommand
                         {
                             if (key.Equals("listener_address"))
                             {
+                                // Check if the value provided is a valid IP address
                                 if (!IPAddress.TryParse(value, out IPAddress ipAddress))
                                 {
                                     vaProxy.WriteToLog($"Invalid listener ip address {value}, using default 127.0.0.1", "red");
@@ -162,7 +204,7 @@ namespace WhisperAttackServerCommand
                             {
                                 try
                                 {
-
+                                    // Check if the listener port is a valid number
                                     _listenerPort = int.Parse(value);
                                 }
                                 catch
@@ -188,6 +230,12 @@ namespace WhisperAttackServerCommand
             }
         }
 
+        /// <summary>
+        /// Start the TCP listener for receiving text commands.
+        /// This is run as a Task so that it does not block the main plugin thread while listening.
+        /// </summary>
+        /// <param name="vaProxy">The VoiceAttack proxy for calling VoiceAttack functions</param>
+        /// <returns></returns>
         private static async Task StartCommandListener(dynamic vaProxy)
         {
             vaProxy.WriteToLog($"Starting WhisperAttack listener on {_listenerIpAddress}:{_listenerPort}", "blue");
@@ -210,6 +258,13 @@ namespace WhisperAttackServerCommand
             }
         }
 
+        /// <summary>
+        /// Handler for commands received by listener and then execute the associated VoiceAttack command.
+        /// Run as a Task so that it does not block the main plugin thread.
+        /// </summary>
+        /// <param name="vaProxy">The VoiceAttack proxy for calling VoiceAttack functions</param>
+        /// <param name="client">The TCP client to read from the received command from</param>
+        /// <returns></returns>
         private static async Task HandleWhisperAttackCommand(dynamic vaProxy, TcpClient client)
         {
             await Task.Yield();
@@ -224,6 +279,9 @@ namespace WhisperAttackServerCommand
 
                     vaProxy.WriteToLog($"Received WhisperAttack command: '{receivedMessage}'", "grey");
 
+                    // If the command exists within the VoiceAttack profile then run it.
+                    // We check for the command first so that we can log this nicely vs. the generic
+                    // external command failure.
                     if (vaProxy.Command.Exists(receivedMessage))
                     {
                         vaProxy.Command.Execute(receivedMessage, true, true);
